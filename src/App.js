615632d1d1b18f980277ac2ae0bcaf88f4d511f4
hectoralfonso <hectoralfonso@penguin>
2019-10-29 12:39:14 -0600

    useState ready

--- src/App.js
+++ src/App.js
@@ -5,118 +5,147 @@ import Question from "./components/Question";
 import Answers from "./components/Answers";
 
 function App() {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [currentAnswer, setCurrentAnswer] = useState("");
-    const [answers, setAnswers] = useState([]);
-    const [error, setError] = useState("");
-    const [results, setResults] = useState(false);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [currentAnswer, setCurrentAnswer] = useState("");
+	const [answers, setAnswers] = useState([]);
+	const [error, setError] = useState("");
+	const [results, setResults] = useState(false);
 
-    const questions = [
-        {
-            id: 1,
-            question: "Which statement about Hooks is not true?",
-            answer_a:
-                "Hooks are 100% backwards-compatible and can be used side by side with classes",
-            answer_b: "Hooks are still in beta and not available yet",
-            answer_c:
-                "Hooks are completely opt-in, there's no need to rewrite existing code",
-            answer_d: "All of the above",
-            correct_answer: "b"
-        },
-        {
-            id: 2,
-            question: "Which one is not a Hook?",
-            answer_a: "useState()",
-            answer_b: "useConst()",
-            answer_c: "useReducer()",
-            answer_d: "All of the above",
-            correct_answer: "b"
-        },
-        {
-            id: 3,
-            question: "What Hook should be used for data fetching?",
-            answer_a: "useDataFetching()",
-            answer_b: "useApi()",
-            answer_c: "useEffect()",
-            answer_d: "useRequest()",
-            correct_answer: "c"
-        }
-    ];
+	const questions = [
+		{
+			id: 1,
+			question: "Which statement about Hooks is not true?",
+			answer_a:
+				"Hooks are 100% backwards-compatible and can be used side by side with classes",
+			answer_b: "Hooks are still in beta and not available yet",
+			answer_c:
+				"Hooks are completely opt-in, there's no need to rewrite existing code",
+			answer_d: "All of the above",
+			correct_answer: "b"
+		},
+		{
+			id: 2,
+			question: "Which one is not a Hook?",
+			answer_a: "useState()",
+			answer_b: "useConst()",
+			answer_c: "useReducer()",
+			answer_d: "All of the above",
+			correct_answer: "b"
+		},
+		{
+			id: 3,
+			question: "What Hook should be used for data fetching?",
+			answer_a: "useDataFetching()",
+			answer_b: "useApi()",
+			answer_c: "useEffect()",
+			answer_d: "useRequest()",
+			correct_answer: "c"
+		}
+	];
 
-    const question = questions[currentQuestion];
+	const question = questions[currentQuestion];
 
-    const renderError = () => {
-        if (!error) {
-            return;
-        }
+	/*
+	
+	These are the functions that are later passed as events and data handling
 
-        return <div className="error">{error}</div>;
-    };
+	*/
 
-    const handleClick = e => {
-        setCurrentAnswer(e.target.value);
+	const renderResultsMark = (question, answer) => {
+		if (question.correct_answer === answer.answer) {
+			return <span className="correct">Correct</span>;
+		}
 
-        // Once user's clicked on an answer, setting error back to empty string
-        setError("");
-    };
+		return <span className="failed">Failed</span>;
+	};
 
-    const next = () => {
-        const answer = { questionId: question.id, answer: currentAnswer };
+	const renderResultsData = () => {
+		return answers.map(answer => {
+			const question = questions.find(
+				question => question.id === answer.questionId
+			);
 
-        if (!currentAnswer) {
-            setError("Please select an option");
-            return;
-        }
+			return (
+				<div key={question.id}>
+					{question.question} - {renderResultsMark(question, answer)}
+				</div>
+			);
+		});
+	};
 
-        answers.push(answer);
-        setAnswers(answers);
-        setCurrentAnswer("");
+	const renderError = () => {
+		if (!error) {
+			return;
+		}
 
-        if (currentQuestion + 1 < questions.length) {
-            setCurrentQuestion(currentQuestion + 1);
-            console.log(currentQuestion);
-            return;
-        }
+		return <div className="error">{error}</div>;
+	};
 
-        setResults(true);
-    };
+	const handleClick = e => {
+		setCurrentAnswer(e.target.value);
 
-    const restart = () => {
-        setResults(false);
-        setCurrentAnswer("");
-        setAnswers([]);
-        setCurrentQuestion(0);
-    };
+		// Once user's clicked on an answer, setting error back to empty string
+		setError("");
+	};
 
-    if (results) {
-        return (
-            <div className="container results">
-                <h2>Results</h2>
-                <button className="btn btn-primary" onClick={restart}>
-                    Restart the quiz
-                </button>
-            </div>
-        );
-    } else {
-        return (
-            <div className="container">
-                <Progress
-                    total={questions.length}
-                    current={currentQuestion + 1}
-                />
-                <Question question={question.question} />
-                {renderError()}
-                <Answers
-                    question={question}
-                    currentAnswer={currentAnswer}
-                    handleClick={handleClick}
-                />
-                <button className="btn btn-primary" onClick={next}>
-                    Confirm and continue
-                </button>
-            </div>
-        );
-    }
+	const next = () => {
+		const answer = { questionId: question.id, answer: currentAnswer };
+
+		if (!currentAnswer) {
+			setError("Please select an option");
+			return;
+		}
+
+		answers.push(answer);
+		setAnswers(answers);
+		setCurrentAnswer("");
+
+		if (currentQuestion + 1 < questions.length) {
+			setCurrentQuestion(currentQuestion + 1);
+			console.log(currentQuestion);
+			return;
+		}
+
+		setResults(true);
+	};
+
+	const restart = () => {
+		setResults(false);
+		setCurrentAnswer("");
+		setAnswers([]);
+		setCurrentQuestion(0);
+	};
+
+	if (results) {
+		return (
+			<div className="container results">
+				<h2>Results</h2>
+				<ul>{renderResultsData()}</ul>
+				<button className="btn btn-primary" onClick={restart}>
+					Restart the quiz
+				</button>
+			</div>
+		);
+	} else {
+		return (
+			<div className="container">
+				<Progress
+					total={questions.length}
+					current={currentQuestion + 1}
+				/>
+				<Question question={question.question} />
+				{renderError()}
+				<Answers
+					question={question}
+					currentAnswer={currentAnswer}
+					handleClick={handleClick}
+				/>
+				<button className="btn btn-primary" onClick={next}>
+					Confirm and continue
+				</button>
+			</div>
+		);
+	}
 }
 
 export default App;
